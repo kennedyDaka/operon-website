da5cae9891937be071a20d6467d306b6e962692c
Kennedy <Kennedydaka93@gmail.com>
2026-06-23 13:22:22 +0200

    Fix menu: use onPointerDown for instant mobile response

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -46,7 +46,10 @@ export function SiteHeader() {
           <button
             type="button"
             aria-label="Toggle menu"
-            onClick={() => setOpen((o) => !o)}
+            onPointerDown={(e) => {
+              e.preventDefault();
+              setOpen((o) => !o);
+            }}
             className="inline-flex h-10 w-10 items-center justify-center border border-white/20 text-white lg:hidden"
           >
             {open ? <X className="h-4 w-4" /> : <Menu className="h-4 w-4" />}
